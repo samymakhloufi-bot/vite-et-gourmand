3556a0ffb930bc90434d8a28ace74f4f00571461
Kremade <samymakhloufi@gmail.com>
2026-06-03 14:47:45 +0200

    mise en ligne : fix BASE_URL local/prod

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,7 @@
         <title> <?php echo $activePage ?? ' Traiteur Bordelais Vite & Gourmand'; ?> </title>
         <meta name="description" content="Julie et José vous proposent une cuisine artisanale de saison pour sublimer vos événements. Livraison dans toute la métropole bordelaise.">
         <meta charset="utf-8"/>
-        <link href="<?= BASE_URL ?>css/style.css" rel="stylesheet" type="text/css">
+        <link href="<?= BASE_URL ?>/css/style.css" rel="stylesheet" type="text/css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
     </head>
